b0983c66ec5c0db36e7418635ac0edf46d19d6f1
Maxim Khitrov <max@mxcrypt.com>
2018-06-11 12:57:47 -0400

    Revert to simple RandBytes implementation

--- rand.go
+++ rand.go
@@ -1,43 +1,12 @@
 package fast
 
-import (
-	"crypto/rand"
-	"sync"
-)
-
-// randPool contains pages of random bytes.
-var randPool = sync.Pool{New: func() interface{} { return new(randPage) }}
+import "crypto/rand"
 
 // RandBytes returns b after filling it with random bytes from a CSPRNG.
 func RandBytes(b []byte) []byte {
-	if len(b) > len((*randPage)(nil).r)/2 {
-		randRead(b)
-	} else {
-		p := randPool.Get().(*randPage)
-		p.copy(b)
-		randPool.Put(p)
-	}
-	return b
-}
-
-// randPage contains random bytes.
-type randPage struct {
-	n int
-	r [4088]byte
-}
-
-// copy transfers random bytes from p to b.
-func (p *randPage) copy(b []byte) {
-	if p.n < len(b) {
-		randRead(p.r[p.n:])
-		p.n = len(p.r)
-	}
-	p.n -= copy(b, p.r[p.n-len(b):p.n])
-}
-
-// randRead fills b with random bytes from crypto/rand.
-func randRead(b []byte) {
+	// Keeping this simple for now, maybe optimize later
 	if _, err := rand.Read(b); err != nil {
 		panic("fast: crypto/rand error: " + err.Error())
 	}
+	return b
 }
